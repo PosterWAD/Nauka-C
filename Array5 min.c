6e45580f96bd2fff9261563bfac54267b365779f
Łukasz Bułdak <posterwad@gmail.com>
2026-01-17 15:23:44 +0100

    Update

--- Array5 min.c
+++ Array5 min.c
@@ -13,7 +13,7 @@ int main() {
     
     int min = tablica[0]; // Zakładam, że pierwsza podana wartość jest najmniejsza, ale kod zaraz sam sprawdzi, która tak na prawdę jest
     
-    for (int i=0; i < 5; i++)
+    for (int i=1; i < 5; i++) // Zaczynałem wcześniej od i = 0 co oznaczało, że w pierwszym ruchu pętla sprawdza: "Czy tablica[0] jest mniejsza od tablicy[0]?
     {
             if (tablica[i] < min)
             {
