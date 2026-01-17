26062445140581d38bb2df35b7b3fa28f18d0152
Łukasz Bułdak <posterwad@gmail.com>
2026-01-17 15:21:57 +0100

    Tablica 5 używająca min

---
+++ Array5 min.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int tablica[5];
+
+    for (int i = 0; i < 5; i++) // Tutaj tylko wpisuję liczby do tablica[5]
+    {
+        printf("Podaj liczby:");
+        scanf("%i", &tablica[i]);
+        
+    }
+    
+    int min = tablica[0]; // Zakładam, że pierwsza podana wartość jest najmniejsza, ale kod zaraz sam sprawdzi, która tak na prawdę jest
+    
+    for (int i=0; i < 5; i++)
+    {
+            if (tablica[i] < min)
+            {
+                min = tablica[i];
+            }
+    }
+
+    printf("Najmniejsza podana liczba to: %i", min);
+    return 0;
+}
