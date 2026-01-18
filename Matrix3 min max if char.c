1eb599df5e7c73ce9a03e6171109f1d274c14c5e
Łukasz Bułdak <posterwad@gmail.com>
2026-01-18 11:58:23 +0100

    Macierz 3

---
+++ Matrix3 min max if char.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+   int A, B;
+   char Z = 'z';
+   
+    printf("Podaj wymiary swojej macierzy\n");
+    printf("Podaj liczbę wierszy swojej macierzy: ");
+    scanf("%i", &A);
+    printf("Podaj liczbę kolumn swojej macierzy: ");
+    scanf("%i", &B);
+   
+   int macierz[A][B];
+   
+   
+    for (int i = 0; i < A; i++)
+    {
+        
+        for (int j = 0; j < B; j++)
+        {   
+            printf("Podaj [%i][%i] element swojej macierzy: ", i + 1, j + 1);
+            scanf("%i", &macierz[i][j]);
+        }
+    }
+    
+    
+        printf("Oto twoja macierz:\n");
+    
+    for (int i = 0; i < A; i++)
+    {
+        
+        for (int j = 0; j < B; j++)
+        {   
+            if (macierz[i][j] < 0) 
+            printf("%c", Z);
+            else printf("%i", macierz[i][j]);
+            
+            printf("\t");
+        }
+        printf("\n");
+    }
+    
+    
+    
+    return 0;
+}
