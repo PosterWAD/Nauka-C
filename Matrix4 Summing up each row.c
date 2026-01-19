a8f13c8f2dcbf67f7ad1769961713cd1e5ded786
Łukasz Bułdak <posterwad@gmail.com>
2026-01-19 22:12:12 +0100

    Update

--- Matrix4 Summing up each row.c
+++ Matrix4 Summing up each row.c
@@ -1,26 +1,5 @@
 #include <stdio.h>
 
-
-int zagadka(int k)
-{
-	if (k <= 0)
-		return -1;
-
-	if (k % 4 == 1)
-		return 1;
-
-	if (k % 4 == 3)
-		return 0;
-
-	if (k % 2 == 0)
-		return zagadka(k / 2);
-
-
-
-}
-
-
-
 int main()
 {
 	int W, K;
