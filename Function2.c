dce9a78542e0c90893041b28ad39591824105b6d
Łukasz Bułdak <posterwad@gmail.com>
2026-01-19 15:28:59 +0100

    Funcja 2

---
+++ Function2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int dodaj(int a, int b)
+{
+ int suma = a + b;
+ return suma;
+ 
+}
+
+int main (void) 
+{
+ int x=2, y =2;
+
+ printf("%i", dodaj(x, y));
+
+
+    return 0;
+}
