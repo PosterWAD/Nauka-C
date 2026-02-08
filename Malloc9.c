88fae2c0e692fcbdefdf45236679c396c3103201
Łukasz Bułdak <posterwad@gmail.com>
2026-02-08 19:31:02 +0100

    Malloc 9

---
+++ Malloc9.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int N;
+    
+    printf("Podaj liczbę: ");
+    scanf("%i", &N);
+    
+    int **tab = malloc(N * sizeof(int*));
+    
+    for (int i = 0; i < N; i++){
+        int x = N + i;
+        
+        tab[i] = malloc(x * sizeof(int));
+        
+        for (int j = 0; j < (i + 1); j++){
+            tab[i][j] = i + 1;
+            printf("%i ", tab[i][j]);
+        }
+        
+        printf("\n");
+    }
+    
+    for (int i = 0; i < N; i++){
+        free(tab[i]);
+    }
+    
+    free(tab);
+    tab = NULL;
+    
+    return 0;
+}
