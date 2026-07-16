8e8d3988ef1966849bea509d5eba890ae66a4f8b
Łukasz Bułdak <posterwad@gmail.com>
2026-07-16 23:36:46 +0200

    .

---
+++ SimpleNumberSwapOnPointers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void zamien(int *x, int *y) {
+    int temp;
+    temp = *y;
+    *y = *x;
+    *x = temp;
+}
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    printf("Przed zamiana: a = %d, b = %d\n", a, b);
+
+    // Wywołanie funkcji. Znak '&' oznacza adres zmiennej.
+    zamien(&a, &b);
+
+    printf("Po zamianie:   a = %d, b = %d\n", a, b);
+
+    return 0;
+}
