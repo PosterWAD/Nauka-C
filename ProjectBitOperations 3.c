d2267ae33969e2f1c231f35ce12072d7f5d3b04d
Łukasz Bułdak <posterwad@gmail.com>
2026-07-18 12:55:17 +0200

    update

--- ProjectBitOperations 3.c
+++ ProjectBitOperations 3.c
@@ -18,9 +18,9 @@ unsigned char sprawdz_oknaPietro(unsigned char okna) {
         else return 0;
 }
 unsigned char sprawdz_oknaOba(unsigned char okna) {
-        if (okna & (sprawdz_oknaParter(okna) & sprawdz_oknaPietro(okna))) {
-            printf("Alarm! Okna na parterze i pietrze są otwarte!\n");
+        if ((sprawdz_oknaParter(okna) & sprawdz_oknaPietro(okna))) {
             uruchom_alarm();
+            printf("Alarm! Okna na parterze i pietrze są otwarte!\n");
             return 1;
         }
         else {
@@ -31,10 +31,19 @@ unsigned char sprawdz_oknaOba(unsigned char okna) {
 
 
 int main() {
-    unsigned char okna = 0b01000000;    // 0 - okno zamkniete, 1 - okno otwarte
+    unsigned char okna = 0b01001000;    // 0 - okno zamkniete, 1 - okno otwarte
     unsigned char sprawdzParter = sprawdz_oknaParter(okna);    
     unsigned char sprawdzPietro = sprawdz_oknaPietro(okna); 
     unsigned char sprawdzOba = sprawdz_oknaOba(okna);
+    unsigned int liczbaOtwartychOkien = 0;
+    
+    for (int i = 0; i < 8; i++) {
+            if (okna & (1 << i)) {
+                printf("Okno %d jest otwarte.\n", i + 1);
+                liczbaOtwartychOkien++;
+            }
+        }
+    printf("Liczba otwartych okien: %i", liczbaOtwartychOkien);
     
     return 0;
 }
