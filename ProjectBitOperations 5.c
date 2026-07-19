66a85143a6e857540570957653d2e9cccf07170d
Łukasz Bułdak <posterwad@gmail.com>
2026-07-19 16:19:56 +0200

    update

--- ProjectBitOperations 5.c
+++ ProjectBitOperations 5.c
@@ -8,12 +8,14 @@ struct Postac {
 };
 
 int main() {
+    // Inicjalizacja postaci
     struct Postac gracz1 = {"Gracz1", 50, 100, 10};
+    struct Postac boss = {"Boss", 200, 50, 25};
+
     int *wskaznikHP = &gracz1.hp;
     int *wskaznikMana = &gracz1.mana;
     int *wskaznikObrazenia = &gracz1.obrazenia;
-    
-    struct Postac boss = {"Boss", 200, 50, 25};
+
     printf("HP przed leczeniem: %d\n", gracz1.hp);
     printf("Mana przed leczeniem: %d\n", gracz1.mana);
     printf("Obrazenia gracz1: %d\n", gracz1.obrazenia);
@@ -25,6 +27,6 @@ int main() {
 
     printf("HP po leczeniu: %d\n", gracz1.hp);
     printf("Mana po leczeniu: %d\n", gracz1.mana);
-    
+
     return 0;
 }
