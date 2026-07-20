49b976de2aaf91a9591e3814036cd648e5699d98
Łukasz Bułdak <posterwad@gmail.com>
2026-07-20 21:48:11 +0200

    update

--- ProjectBitOperations 5.c
+++ ProjectBitOperations 5.c
@@ -16,22 +16,22 @@ unsigned char on_off_wentylator2(unsigned char PORT_A) {
     if ((PORT_A & 0b00000010) == 0b00000000 ) {
         return (PORT_A | 2);
     }
-    else return (PORT_A & ~2);
+    return (PORT_A & ~2);
 }
 unsigned char on_off_wentylator3(unsigned char PORT_A) {
     return ((PORT_A & 0b00000010) == 0b00000000 )? (PORT_A | 2) : (PORT_A & ~2);
 }
 unsigned char on_off_wentylator4(unsigned char PORT_A) {
     return (PORT_A ^ 0b00000010);
-
-}
+} //PODOBNO NAJSZYBSZE 
 
 int main() {
     unsigned char PORT_A = 0b00000000;
     unsigned char Wentylator = 0b00000010;
-    on_off_wentylator1(PORT_A, Wentylator);
-    on_off_wentylator2(PORT_A);
-    on_off_wentylator3(PORT_A);
-    on_off_wentylator4(PORT_A);
+    PORT_A = on_off_wentylator1(PORT_A, Wentylator);
+    PORT_A = on_off_wentylator2(PORT_A);
+    PORT_A = on_off_wentylator3(PORT_A);
+    PORT_A = on_off_wentylator4(PORT_A);
+
     return 0;
 }
