0ded5f9bccbf3938821e096dd067c760073a7232
Łukasz Bułdak <posterwad@gmail.com>
2026-07-21 16:49:10 +0200

    Projekt 6

---
+++ ProjectBitOperations 6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define POMPA (1<<0)
+#define WENTYLATOR (1<<1)
+#define SWIATLA (1<<2)
+#define SYRENA (1<<7)
+
+
+int main() {
+    unsigned char PORT_A = 0b00000000;
+    
+    return 0;
+}
