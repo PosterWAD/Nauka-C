fd8265c551bfaf3369fd0dd3f7a6aef3fe957f46
Łukasz Bułdak <posterwad@gmail.com>
2026-07-22 18:42:21 +0200

    update

--- ProjectBitOperations 6.c
+++ ProjectBitOperations 6.c
@@ -20,12 +20,25 @@ int czy_wlaczone(unsigned char port, unsigned char urzadzenie) {
 
 int main() {
     unsigned char PORT_A = 0b00000000;
-    unsigned char PORT_B = 0b00000000;
+    unsigned char PORT_B = 0b00000001;
 
     PORT_A = wlacz(PORT_A, POMPA | SYRENA);
     PORT_A = wylacz(PORT_A, WENTYLATOR | SWIATLA);
     PORT_A = (PORT_A | (POMPA | SYRENA)) & ~(WENTYLATOR | SWIATLA);
     printf("%d\n", czy_wlaczone(PORT_B, CZUJNIK_DYMU));
+
+
+    while(1) {
+        if (czy_wlaczone(PORT_B, CZUJNIK_DYMU)) {
+        PORT_A = (PORT_A | (POMPA | SYRENA)) & ~(WENTYLATOR | SWIATLA);
+        }
+        else {
+            PORT_A = (PORT_A | (WENTYLATOR | SWIATLA)) & ~(POMPA | SYRENA);
+        }
+    }
+
+
+
 
     return 0;
 }
