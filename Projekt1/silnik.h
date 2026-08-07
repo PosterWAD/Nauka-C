f28b80b25832dd3aa0ae9d0676515f162de1016e
Łukasz Bułdak <posterwad@gmail.com>
2026-08-07 21:38:16 +0200

    update

--- Projekt1/silnik.h
+++ Projekt1/silnik.h
@@ -3,9 +3,10 @@
 
 struct Silnik {
     unsigned int predkosc;      // Prędkość w km/h
-    unsigned int obroty_rpm;    // Obroty silnika na minutę
-    int temperatura_silnika;    // Temperatura w stopniach Celsjusza 
+    unsigned int obroty_rpm;
+    int temperatura_silnika;    // Temperatura w stopniach C 
     int tryb_awaryjny;          //1 = awaria/przegrzanie
 };
+void aktualizuj_silnik(struct Silnik *wskaznik_na_silnik);
 
 #endif // SILNIK_H
