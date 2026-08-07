e6739979726ae7a352a8670d63c6b6a7462f0c6e
Łukasz Bułdak <posterwad@gmail.com>
2026-08-08 00:12:37 +0200

    update

--- Projekt1/main.c
+++ Projekt1/main.c
@@ -12,6 +12,37 @@ int main() {
     printf("Obroty: %d RPM\n", auto.obroty_rpm);
     printf("Bieg: %d\n", auto.bieg);
 
+    while(1) {
+        char wybor;
+        printf("\nWybierz akcję:\n");
+        printf("1. Wcisnij gaz\n");
+        printf("2. Wcisnij hamulec\n");
+        printf("3. Wyjdz\n");
+        printf("Twoj wybor: ");
+        scanf(" %c", &wybor);
+
+        switch(wybor) {
+            case '1':
+                wcisnij_gaz(&auto);
+                break;
+            case '2':
+                wcisnij_hamulec(&auto);
+                break;
+            case '3':
+                printf("Wychodznie...\n");
+                return 0;
+            default:
+                printf("Nieprawidlowy wybor. Sprobuj ponownie.\n");
+                continue;
+        }
+
+        aktualizuj_silnik(&auto);
+
+        printf("\nAktualny stan silnika:\n");
+        printf("Predkosc: %d km/h\n", auto.predkosc);
+        printf("Obroty: %d RPM\n", auto.obroty_rpm);
+        printf("Bieg: %d\n", auto.bieg);
+    }
     
     return 0;
 }
