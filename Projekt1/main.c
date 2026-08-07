300f9e2149c16b738b45317481d8c46d1da55494
Łukasz Bułdak <posterwad@gmail.com>
2026-08-08 00:12:11 +0200

    update

--- Projekt1/main.c
+++ Projekt1/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "silnik.h"
-#include "silnik.c"
-int main() {
+#include "silnik.h" 
 
+int main() {
+    struct Silnik auto;
+
+    wlacz_zaplon(&auto);
+
+    printf("Silnik odpalony!\n");
+    printf("Predkosc: %d km/h\n", auto.predkosc);
+    printf("Obroty: %d RPM\n", auto.obroty_rpm);
+    printf("Bieg: %d\n", auto.bieg);
+
+    
     return 0;
 }
