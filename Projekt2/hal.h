9f285e46119d9230d95fcf1708fddadc12ee9c47
Łukasz Bułdak <posterwad@gmail.com>
2026-08-16 22:42:17 +0200

    update

--- Projekt2/hal.h
+++ Projekt2/hal.h
@@ -9,5 +9,12 @@ void HAL_WylaczPompe(void);
 void HAL_WlaczAlarm(void);
 void HAL_WylaczAlarm(void);
 
+// Odczyt wejść
+unsigned int HAL_CzytajCisnienie(void);
+
+
+void HAL_SymulujCzujnik(unsigned int nowe_cisnienie);
+void HAL_WypiszStanGlownegoPortu(void);
+
 
 #endif // HAL_H
