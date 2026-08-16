9a18fef20b0a9a04e9a24256b7befe5e9552b765
Łukasz Bułdak <posterwad@gmail.com>
2026-08-16 23:21:16 +0200

    update

--- Projekt2/kontroler.c
+++ Projekt2/kontroler.c
@@ -7,3 +7,39 @@ void Kontroler_Inicjalizacja(struct KontrolerProcesu *k) {
     k->limit_cisnienia_min = 500;
 }
 
+void Kontroler_Aktualizuj(struct KontrolerProcesu *k) {
+    unsigned int obecne_cisnienie = HAL_CzytajCisnienie();
+
+    switch (k->aktualny_stan) {
+        
+        case STAN_INICJALIZACJA:
+            HAL_WylaczPompe();
+            HAL_WylaczAlarm();
+            k->aktualny_stan = STAN_PRACA_NORMALNA;
+            break;
+
+        case STAN_PRACA_NORMALNA:
+            if (obecne_cisnienie > k->limit_cisnienia_max) {
+                HAL_WylaczPompe();
+                HAL_WlaczAlarm();
+                k->aktualny_stan = STAN_AWARIA_CISNIENIA;
+            } 
+            else if (obecne_cisnienie < k->limit_cisnienia_min) {
+                HAL_WlaczPompe(); 
+            }
+            else {
+                HAL_WylaczPompe(); 
+            }
+            break;
+
+        case STAN_AWARIA_CISNIENIA:
+            HAL_WylaczPompe();
+            HAL_WlaczAlarm();
+            
+            if (obecne_cisnienie < (k->limit_cisnienia_max - 200)) {
+                HAL_WylaczAlarm();
+                k->aktualny_stan = STAN_PRACA_NORMALNA;
+            }
+            break;
+    }
+}
