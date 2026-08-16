9e8ec76e4bcabaf37a615f42a7280e647044d9a0
Łukasz Bułdak <posterwad@gmail.com>
2026-08-16 22:41:24 +0200

    update

--- Projekt2/hal.h
+++ Projekt2/hal.h
@@ -4,4 +4,10 @@
 // Inicjalizacja
 void HAL_Inicjalizacja(void);
 
+void HAL_WlaczPompe(void);
+void HAL_WylaczPompe(void);
+void HAL_WlaczAlarm(void);
+void HAL_WylaczAlarm(void);
+
+
 #endif // HAL_H
