4ab10873e02703ecc7485b7f0bea0f26ec6e95f0
Łukasz Bułdak <posterwad@gmail.com>
2026-08-16 23:16:51 +0200

    update

--- Projekt2/kontroler.h
+++ Projekt2/kontroler.h
@@ -7,3 +7,13 @@ typedef enum {
     STAN_PRACA_NORMALNA,
     STAN_AWARIA_CISNIENIA
 } StanSystemu;
+
+// Struktura opisująca parametry całego układu
+struct KontrolerProcesu {
+    StanSystemu aktualny_stan;
+    unsigned int limit_cisnienia_max;
+    unsigned int limit_cisnienia_min;
+};
+
+
+#endif // KONTROLER_H
