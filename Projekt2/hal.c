d329ca7047af9a9896ea87ecb549ddac0a742c7b
Łukasz Bułdak <posterwad@gmail.com>
2026-08-16 22:43:43 +0200

    update

--- Projekt2/hal.c
+++ Projekt2/hal.c
@@ -6,3 +6,12 @@
 
 static unsigned char REJESTR_WYJSCIA = 0x00;
 static unsigned int  REJESTR_ADC_CISNIENIE = 0;
+
+void HAL_Inicjalizacja(void) {
+    REJESTR_WYJSCIA = 0x00;
+    REJESTR_ADC_CISNIENIE = 1000;
+}
+
+void HAL_WlaczPompe(void) {
+    REJESTR_WYJSCIA |= PIN_POMPA;
+}
