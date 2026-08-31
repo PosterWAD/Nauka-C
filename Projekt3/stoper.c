79366684b357d9f5a23fff0632311eef0d3d9c4f
Łukasz Bułdak <posterwad@gmail.com>
2026-08-31 14:56:59 +0200

    update

--- Projekt3/stoper.c
+++ Projekt3/stoper.c
@@ -6,3 +6,10 @@ void StoperStart(struct Stoper *stoper) {
     stoper->czas_pracy = 0;
 }
 
+void StoperAktualizuj(struct Stoper *stoper, uint32_t czas_teraz) {
+    if (stoper->ostatni_odczyt != 0) {
+        stoper->czas_pracy += czas_teraz - stoper->ostatni_odczyt;
+
+    }
+    stoper->ostatni_odczyt = czas_teraz;
+}
