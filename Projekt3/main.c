54623ea91d49a541faef6a47896949512d7ac894
Łukasz Bułdak <posterwad@gmail.com>
2026-08-31 14:32:17 +0200

    update

--- Projekt3/main.c
+++ Projekt3/main.c
@@ -5,7 +5,11 @@ int main() {
 
     struct Stoper stoper;
     StoperStart(&stoper);
-    
+
+    while (1) {
+        uint32_t czas_teraz = pobierz_czas();
+        StoperAktualizuj(&stoper, czas_teraz);
+    }
 
     
     return 0;
